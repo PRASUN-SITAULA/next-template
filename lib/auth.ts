7d83ffd4ee20ec6b5fe665ad73dd795750d4de87
PRASUN-SITAULA <psitaula209@gmail.com>
2025-11-24 21:02:26 +0545

    Upgraded better-auth to 1.4.1

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,10 +1,9 @@
-import { betterAuth } from "better-auth"
 import { prismaAdapter } from "better-auth/adapters/prisma"
+import { betterAuth } from "better-auth/minimal"
 import { nextCookies } from "better-auth/next-js"
 import { haveIBeenPwned } from "better-auth/plugins"
-import { PrismaClient } from "@/app/generated/prisma/client"
+import prisma from "./prisma"
 
-const prisma = new PrismaClient()
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
